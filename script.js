d030459f1a47d70824ded3ce6a34c89592a9e00b
mdt-alikhwann <zuliaadzkiyati@gmail.com>
2025-10-10 09:33:49 +0700

    Update script.js

--- script.js
+++ script.js
@@ -710,7 +710,7 @@ onValue(konfirmasiQuery, (snapshot) => {
     const shareBtn = document.getElementById('share');
 
     const frame = new Image();
-    frame.src = '/twbmdt.png'; // pastikan path benar
+    frame.src = './twbmdt.png'; // pastikan path benar
     let frameReady = false;
     frame.onload = () => { frameReady = true; draw(); };
 
@@ -897,4 +897,5 @@ https://domainmu.com/`;
     }
 
   });
+
 
