7e163305775a0cd2eff39c9b2b77688b51f01136
mdt-alikhwann <zuliaadzkiyati@gmail.com>
2025-10-14 08:17:19 +0700

    Update music.js

--- assets/js/music.js
+++ assets/js/music.js
@@ -3,7 +3,7 @@ const openBtn = document.getElementById("openBtn");
 const music = document.getElementById("bgMusic");
 const qrBtn = document.getElementById("qrBtn");
 let isMainPlaying = false;
-music.volume = 0.6;
+music.volume = 0.3;
 
 function playMainMusic() {
   music.play();
